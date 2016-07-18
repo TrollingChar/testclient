b11b9020af80b683868dd43367f98f1e3410fb3c
Маргарита <kulikova13vo1@gmail.com>
2016-07-18 15:16:48 +0300

    added PaneArsenal without buttons

--- src/hz/company/testclient/interf/PanelArsenal.hx
+++ src/hz/company/testclient/interf/PanelArsenal.hx
@@ -7,7 +7,7 @@ import hz.company.testclient.interf.Panel;
  */
 class PanelArsenal extends Panel
 {
-
+	
 	public function new() 
 	{
 		
@@ -15,6 +15,13 @@ class PanelArsenal extends Panel
 	
 	function override Resize(event:Event)
 	{
-		
+		var scX:Float = stage.stageWidth / 1000;
+		var scY:Float = stage.stageHeight / 600 * 0.8;
+		var scale:Float = scaleX = scaleY = Math.min(scX, scY);
+		posShown.x = stage.stageWidth / 2 - 500 * scale;
+		posShown.y = stage.stageHeight * .45 - 300 * scale;
+		posHidden.x = posShown.x;
+		posHidden.y = stage.stageHeight;
+		position = position;
 	}
 }
