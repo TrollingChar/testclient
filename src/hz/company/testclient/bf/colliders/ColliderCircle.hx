cf3dfe063679382fac3a2ae1d6033ef5f0221c51
sendername <sendername@yandex.ru>
2016-07-21 12:56:02 +0300

    cirlceCollider - collide with circle impements

--- src/hz/company/testclient/bf/colliders/ColliderCircle.hx
+++ src/hz/company/testclient/bf/colliders/ColliderCircle.hx
@@ -1,4 +1,5 @@
 package hz.company.testclient.bf.colliders;
+import openfl.geom.Point;
 
 /**
  * ...
@@ -6,19 +7,17 @@ package hz.company.testclient.bf.colliders;
  */
 class ColliderCircle extends Collider
 {
-	var offset:Point;
-	var cachePoint:Point;
-	var radius:Float;
+	public var offset:Point;
+	public var cachePoint:Point;
+	public var radius:Float;
 	
 	public function new() 
 	{
-		super();
-		
+		super();		
 	}
 	
 	override public function collideWithPoint(collider:ColliderPoint):Collision {
-		
-		
+				
 	}
 	
 	override public function collideWithLine(collider:ColliderLine):Collision {
@@ -27,6 +26,47 @@ class ColliderCircle extends Collider
 	
 	override public function collideWithCircle(collider:ColliderCircle):Collision {
 		
+		if ((this.radius == 0) && (collider.radius == 0))
+			return null;
+		
+		if ((((collider.cachePoint.x - this.cachePoint.x) * this.object.vx) + 
+		((collider.cachePoint.y - this.cachePoint.y) * this.object.vy)) < 0)
+			return 0;
+		
+		var v:Float = Math.sqrt(this.object.vx * this.object.x +
+		                        this.object.vy * this.object.vy);
+		
+		var a:Float = Math.sqrt( Math.pow((this.cachePoint.x - collider.cachePoint.x), 2) + 
+		                         Math.pow((this.cachePoint.y - collider.cachePoint.y), 2));
+		
+		var b:Float = Math.sqrt( Math.pow((this.cachePoint.x + this.object.vx - collider.cachePoint.x), 2) +
+		                         Math.pow((this.cachePoint.y + this.object.vy - collider.cachePoint.y), 2));
+								 
+		var p:Float = (a + b + v) / 2;
+		var S:Float = Math.sqrt(p * (p - a) * (p - b) * (p - v));
+		var h:Flaot = (2 * S) / v;
+		
+		if ((this.radius + collider.radius) > h)
+			return null;
+		
+		var d:Float = Math.sqrt(a * a - h * h) - Math.sqrt(Math.pow(this.radius + collider.radius) - h * h);
+		
+		if ((d < 0) || (d >= v))
+			return null;
+				                   
+		if (Math.isNaN(d))
+			return null;
+		
+		if (d)
+		{
+			this.cachePoint.x += this.object.vx * d / v;
+			this.cachePoint.y += this.object.vy * d / v;
+		}
+		
+		if ((Math.isNaN(this.cachePoint.x) || (Math.isNaN(collider.cachePoint.x))
+			return null;
+		
+		return new Collision(/* Parametrs */);
 	}
 	
 }
