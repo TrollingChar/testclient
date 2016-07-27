b98e3f5e7f3182552b9b9ffcdf136180f80a25f7
Маргарита <kulikova13vo1@gmail.com>
2016-07-27 11:57:07 +0300

    начали разбор очереди червей

---
+++ src/hz/company/testclient/bf/Team.hx
@@ -0,0 +1,24 @@
+package hz.company.testclient.bf;
+import hz.company.testclient.bf.objects.Worm;
+
+/**
+ * ...
+ * @author 
+ */
+class Team
+{
+	var activeWorm:Worm;
+	var otherWorms:List<Worm>;
+	
+	public function new() 
+	{
+		otherWorms = new List();
+	}
+	
+	public function add(worm:Worm)
+	{
+		
+	}
+	
+	//public function
+}
---
+++ src/hz/company/testclient/bf/objects/Worm.hx
@@ -0,0 +1,16 @@
+package hz.company.testclient.bf.objects;
+
+/**
+ * ...
+ * @author 
+ */
+class Worm extends Object
+{
+	public var hp:Int;
+
+	public function new() 
+	{
+		
+	}
+	
+}
