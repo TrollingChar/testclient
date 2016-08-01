9483dc38af83c3450163a9efb23936c0615e5afe
Маргарита <kulikova13vo1@gmail.com>
2016-08-01 12:22:44 +0300

    перенесен Controller

--- src/hz/company/testclient/bf/controllers/Controller.hx
+++ src/hz/company/testclient/bf/controllers/Controller.hx
@@ -8,10 +8,17 @@ import hz.company.testclient.bf.objects.Object;
 class Controller
 {
 	public var object:Object;
+	public var fuse:Int = 2000;
+	var map:Map;
+	var bounceForbidX:Float,
+	var bounceForbidY:Float,
+	var bounceForbidR:Float;
 
-	public function new() 
+	public function new(object:Object) 
 	{
-		
+		obj = object;
+		map = obj.map;
+		//map.addEventListener(Event.ENTER_FRAME, _work);
 	}
 	
 	public function onAdd()
@@ -29,4 +36,20 @@ class Controller
 		
 	}
 	
+	function fuseCallback() {
+		work();
+	}
+	
+	public function update() {
+		if(fuse-- == 0) 
+			fuseCallback();
+		else
+			work();
+	}
+		
+	public function work() {}
+		
+	public function die() {
+		//map.removeEventListener(Event.ENTER_FRAME, _work);
+	}
 }
